b44e42b959703311bfd57cf1597996294803eab9
Devon DeJohn <ddejohn@pm.me>
2020-03-09 13:50:04 -0600

    primes up to x working

--- hello.hs
+++ hello.hs
@@ -9,22 +9,19 @@ is_prime :: Integer -> Bool
 f x = x * x
 add x y = x + y
 mul x y = x * y
-
 summate [] = 0
 summate lst = (head lst) + summate (tail lst)
-
 divisors x = [n | n <- [1..x], (mod x n) == 0]
-
 is_prime x = length (divisors x) == 2
 
 -- list of primes up to and including x
-primes x = [p!!1 | p <- map divisors [2..x]]
+primes x = [p | p <- [2..x], is_prime p]
 
 main = do
     -- print (f 3)
     -- print (add 2 3)
     -- print (mul 3 8)
     -- print (summate [1..5])
-    print (is_prime 4)
+    -- print (is_prime 4)
     -- print (divisors 114)
-    -- print (primes 113)
+    print (primes 113)
