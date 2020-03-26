eb686b15ebbb210c33854904dd6c36d816ce4d02
Devon DeJohn <ddejohn@pm.me>
2020-03-26 01:00:04 -0600

    oki

--- hello.hs
+++ hello.hs
@@ -1,18 +1,23 @@
 add :: Integer -> Integer -> Integer
 add x y = x + y
 
+
 mul :: Integer -> Integer -> Integer
 mul x y = x * y
+
 
 sum' :: [Integer] -> Integer
 sum' [] = 0
 sum' lst =  add (head lst) (sum' (tail lst))
 
+
 divisors :: Integer -> [Integer]
 divisors x = [n | n <- [1..x], (mod x n) == 0]
 
+
 is_prime :: Integer -> Bool
 is_prime x = length (divisors x) == 2
+
 
 gcd' :: Integer -> Integer -> Integer
 gcd' a b
@@ -20,17 +25,22 @@ gcd' a b
     | a < b = gcd' b a
     | otherwise = gcd' b (mod a b)
 
+
 coprime :: Integer -> Integer -> Bool
 coprime a b = gcd' a b == 1
+
 
 primes :: Integer -> [Integer]
 primes n = [p | p <- filter is_prime [2..n]]
 
+
 totient :: Integer -> Int
 totient m = length [r | r <- [1..m-1], coprime m r]
 
+
 primeFactors :: Integer -> [Integer]
 primeFactors n = [m | m <- divisors n, is_prime m]
+
 
 main = do
     -- print (add 2 3)
