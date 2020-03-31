534bc256eb925e9aa4b6c3f229657b8ece0800f8
Devon DeJohn <ddejohn@pm.me>
2020-03-31 14:24:57 -0600

    type decs above func defs derp

--- nq.hs
+++ nq.hs
@@ -1,47 +1,57 @@
 type Seq = [Char]
 type Board = [Seq]
 
-
-setup :: Int -> Board
-transpose :: Board -> Board
-rows :: Board -> Int
-cols :: Board -> Int
-size :: Board -> Int
-
-qSeq :: Seq -> Int
-qBoard :: Board -> Int
-
-cVal :: Seq -> Bool
-rVal :: Seq -> Bool
-
-allRows :: Board -> Bool
-allCols :: Board -> Bool
-
 -- primaryDiag :: Board -> Int -> [(Int, Int)]
 -- secondaryDiag :: Board -> Int -> [(Int, Int)]
 
+setup :: Int -> Board
 setup n
     | n < 4 = setup 4
     | otherwise = [replicate n '-' | _ <- [1..n]]
 
+
+transpose :: Board -> Board
 transpose b = [[r!!i | r <- b] | i <- [0..length b - 1]]
 
+
+rows :: Board -> Int
 rows b = length b
+
+
+cols :: Board -> Int
 cols b
     | all (== n) [length r | r <- b] = n
     | otherwise = 0
     where n = rows b
+
+
+size :: Board -> Int
 size b
     | (cols b == rows b) = rows b
     | otherwise = 0
 
+
+qSeq :: Seq -> Int
 qSeq seq = sum [1 | q <- seq, q == 'Q']
+
+
+qBoard :: Board -> Int
 qBoard b = sum [qSeq r | r <- b]
 
+
+rVal :: Seq -> Bool
 rVal r = qSeq r < 2
+
+
+cVal :: Seq -> Bool
 cVal c = rVal c
 
+
+allRows :: Board -> Bool
 allRows b = all (== True) [rVal r | r <- b]
+
+
+allCols :: Board -> Bool
 allCols b = allRows (transpose b)
 
 -- main = do
